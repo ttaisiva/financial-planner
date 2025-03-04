3f4f84727121352aa1daa93bfe477294bd3062d6
Sophie Leonhardt <sophie.leonhardt@stonybrook.edu>
2025-03-03 19:12:14 -0500

    added some minor components

---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#dashboard">Dashboard</a></li>
+          <li><a href="#resources">Resources</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
